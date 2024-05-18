c4eb9f569eb909a6f38c1a5c9cce05f49ff57084
fernandoG494 <darkogarcia3@gmail.com>
2024-05-18 14:42:08 -0600

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 import { fromEvent } from "rxjs";
+
+const src1$ = fromEvent(document, 'click');
+const src2$ = fromEvent(document, 'keyup');
+
+const observer = {
+    next: value => console.log('next:', value),
+};
+
+src1$.subscribe(observer);
+src2$.subscribe(observer);

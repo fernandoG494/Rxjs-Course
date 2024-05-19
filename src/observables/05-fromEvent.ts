54c6ca22a7c716ae964f911214c94404e203dacb
fernandoG494 <darkogarcia3@gmail.com>
2024-05-18 18:09:31 -0600

    range

---
+++ src/observables/05-fromEvent.ts
@@ -0,0 +1,14 @@
+import { fromEvent } from "rxjs";
+
+const src1$ = fromEvent<PointerEvent>(document, 'click');
+const src2$ = fromEvent<KeyboardEvent>(document, 'keyup');
+
+const observer = {
+    next: value => console.log('next:', value),
+};
+
+src1$.subscribe(observer);
+src2$.subscribe(event => {
+    console.log(event);
+    console.log(event.key);
+});
---
+++ src/observables/06-range.ts
@@ -0,0 +1,7 @@
+import { asyncScheduler, range } from "rxjs";
+
+const src$ = range(1, 10, asyncScheduler); // async = asyncScheduler
+
+console.log('Inicio');
+src$.subscribe(console.log)
+console.log('Fin');
